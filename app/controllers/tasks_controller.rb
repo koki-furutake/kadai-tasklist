6d9773bc56277be3ef0b4f307890922e34efea19
koki.furutake <koki.furutake5@gmail.co.jp>
2018-02-19 05:37:22 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
     @tasks.destroy
 
     flash[:success] = 'タスクは正常に削除されました'
-    redirect_to messages_url
+    redirect_to tasks_url
   end
 
   private
